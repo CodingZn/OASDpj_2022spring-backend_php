8c13b0b866f7f4bbf553ae2166a553e0d87c8e15
xintianle <20307110196@fudan.edu.cn>
2022-05-23 14:10:24 +0800

    add: method

--- php/cart/cart.php
+++ php/cart/cart.php
@@ -1,15 +1,25 @@
 <?php
 require_once "../header.php";
 require_once "../Mysql.php";
+require_once "../checkToken.php";
 
 $req_method = $_SERVER['REQUEST_METHOD'];
 
 
+if ($req_method == "GET"){//获取购物车里的所有商品
+    if (!checkCustomerToken()) {
+        $data = array("message"=> "无操作权限！");
+        http_response_code(401);
+        exit(json_encode($data));
+    }
 
-if ($req_method == "GET"){
 
 
-}elseif ($req_method == "POST"){
+}elseif ($req_method == "POST"){//将一个商品添加到购物车
+    http_response_code(405);
+}
+elseif ($req_method == "DELETE"){//删除购物车中的一个艺术品
+
     http_response_code(405);
 }
 else{
