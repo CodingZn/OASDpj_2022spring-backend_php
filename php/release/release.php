5f7fbc72a65ccf01ed630f03b7e152b55ace466e
xintianle <20307110196@fudan.edu.cn>
2022-05-24 23:05:33 +0800

    add POST in release.php, with sth to add

--- php/release/release.php
+++ php/release/release.php
@@ -37,7 +37,46 @@ elseif ($req_method == "POST"){
 }
 
     //新增
+
+//获取表单
+    $data = json_decode(file_get_contents('php://input'), true);
+    $mysql = new Mysql();
+
+    if (array_key_exists('ArtistID', $data)){
+        $ArtistID = $data['ArtistID'];
+    }
+    else{//新增一个Artist，仅包括Name
+        $ArtistName=$data['ArtistName'];
+
+//create artist with name
+        $ArtistID=1;
+    }
+    $Title = $data['Title'];
+    $Description = $data['Description'];
+    $YearOfWork = $data['YearOfWork'];
+    $Width = $data['Width'];
+    $Height = $data['Height'];
+    $GenreID = $data['GenreID'];
+    $MSRP = $data['MSRP'];
+
+    $ImageFileName = 1;//generated
+
+    $columnNames = array('ArtistID', 'Title', 'Description',
+        'YearOfWork', 'Width', 'Height', 'GenreID', 'MSRP',
+        'ImageFileName');
+    $columnValues = array($ArtistID, $Title, $Description,
+        $YearOfWork, $Width, $Height, $GenreID, $MSRP,
+        $ImageFileName);
+
+    $result = $mysql->insert('paintings', $columnNames, $columnValues);
+
+    if (!$result){
+        http_response_code(500);
+        exit(json_encode(array('message'=>"未知错误！")));
+    }
+
     http_response_code(200);
+    exit(json_encode(array('message'=>"创建成功！")));
 }
 elseif ($req_method == "PATCH"){
     if(array_key_exists('PaintingID', $_GET))
