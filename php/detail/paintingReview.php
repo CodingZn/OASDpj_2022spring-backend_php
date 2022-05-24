e4d1a459650a4f62428eafa0905b911f8315a3b9
xintianle <20307110196@fudan.edu.cn>
2022-05-24 21:16:15 +0800

    add PATCH, POST, DELETE in paintingReview.php

--- php/detail/paintingReview.php
+++ php/detail/paintingReview.php
@@ -29,9 +29,23 @@ elseif($req_method == "POST"){//创建评论
     }
 
     $data = json_decode(file_get_contents('php://input'), true);
+    $PaintingID=$data['PaintingID'];
+    $comment=$data['Comment'];
+    $mysql=new Mysql();
 
-    //save
+    $columnNames=array('PaintingID', 'CustomerID', 'Comment');
+    $columnValues=array($PaintingID, $userID, $comment);
+    $result=$mysql->insert('reviews', $columnNames, $columnValues);
 
+    if ($result){
+        $data = array("message"=> "评论成功！");
+        exit(json_encode($data));
+    }
+    else {
+        $data = array("message"=> "未知错误！");
+        http_response_code(500);
+        exit(json_encode($data));
+    }
 }
 elseif($req_method == "PATCH"){//点赞或取消
     $userID = checkCustomerToken();
@@ -48,23 +62,60 @@ elseif($req_method == "PATCH"){//点赞或取消
 
     $mysql=new Mysql();
     $review = $mysql->selectAReview($RatingID);
+    $rating=$review->Rating;
     if ($RatingOP > 0)
-        $review->Rating = $review->Rating + 1;
-    else
-        $review->Rating = $review->Rating - 1;
+        $rating = $rating + 1;
+    else{
+        if ($rating > 0)
+            $rating->Rating = $rating - 1;
+    }
 
-    //save review
+    //save
+    $columnNames=array('Rating');
+    $columnValues=array($rating);
+    $mysql->insert('reviews', $columnNames, $columnValues);
+
+    exit();
 
 }
 elseif($req_method == "DELETE"){//删除评论
+    //必要参数
+    if (array_key_exists('RatingID',$_GET)){
+        $RatingID = $_GET['RatingID'];
+    }
+    else {
+        $data = array("message" => "缺少必要参数！");
+        http_response_code(400);
+        exit(json_encode($data));
+    }
     $userID = checkCustomerToken();
     if (!$userID) {
         $data = array("message"=> "无操作权限！");
         http_response_code(401);
         exit(json_encode($data));
     }
+    $mysql=new Mysql();
 
+    //检查权限
+    $result = $mysql->select("RatingID", 'reviews',
+        "WHERE RatingID='$RatingID'");
+    if (!$result){
+        http_response_code(400);
+        $data=array('message'=>"该评论不存在！");
+        exit(json_encode($data));
+    }
+    $result = $mysql->select("RatingID", 'reviews',
+        "WHERE RatingID='$RatingID' AND CustomerID='$userID");
+    if (!$result){
+        http_response_code(403);
+        $data=array('message'=>"不能删除别人的评论！");
+        exit(json_encode($data));
+    }
 
+    $mysql->delete('RatingID', "WHERE RatingID='$RatingID'");
+
+    $data=array('message'=>"删除成功！");
+    exit(json_encode($data));
 }
 else{
     http_response_code(405);
