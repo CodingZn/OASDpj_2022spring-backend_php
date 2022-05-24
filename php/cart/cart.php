55a7a9cd1331b8e0ce54f6a336b4f102c9f27d0a
xintianle <20307110196@fudan.edu.cn>
2022-05-24 20:52:29 +0800

    add POST, DELETE in cart.php

--- php/cart/cart.php
+++ php/cart/cart.php
@@ -26,7 +26,33 @@ if ($req_method == "GET"){//获取购物车里的所有商品
     exit(json_encode($data));
 }
 elseif ($req_method == "POST"){//将一个商品添加到购物车
-    http_response_code(200);
+    $mysql = new Mysql();
+//获取表单
+    $data = json_decode(file_get_contents('php://input'), true);
+    $PaintingID = $data['PaintingID'];
+
+    $columnNames=array('CustomerID', 'PaintingID');
+    $columnValues=array($userID, $PaintingID);
+//检查是否已经添加到购物车
+    $result = $mysql->select($columnNames, 'customer_cart',
+        "WHERE CustomerID='$userID' AND PaintingID='$PaintingID'");
+    if ($result){
+        http_response_code(400);
+        $data=array('message'=>"您已添加过该艺术品，不能重复添加！");
+        exit(json_encode($data));
+    }
+//添加到购物车
+    $result = $mysql->insert('customer_cart', $columnNames, $columnValues);
+    if ($result){
+        http_response_code(200);
+        $data=array('message'=>"添加成功！");
+        exit(json_encode($data));
+    }
+    else{
+        http_response_code(500);
+        exit(json_encode(array('message'=>"未知错误！")));
+    }
+
 }
 elseif ($req_method == "DELETE"){//删除购物车中的一个艺术品
     if (array_key_exists('PaintingID',$_GET)){
@@ -37,8 +63,26 @@ elseif ($req_method == "DELETE"){//删除购物车中的一个艺术品
         http_response_code(400);
         exit(json_encode($data));
     }
+    $mysql=new Mysql();
 
-    http_response_code(200);
+    //检查是否已经添加到购物车
+    $result = $mysql->select("*", 'customer_cart',
+        "WHERE CustomerID='$userID' AND PaintingID='$PaintingID'");
+    if (!$result){
+        http_response_code(400);
+        $data=array('message'=>"您的购物车里没有该商品！");
+        exit(json_encode($data));
+    }
+    $result=$mysql->delete('customer_cart', "WHERE CustomerID='$userID' AND PaintingID='$PaintingID'");
+    if ($result){
+        http_response_code(200);
+        $data=array('message'=>"删除成功！");
+        exit(json_encode($data));
+    }
+    else{
+        http_response_code(500);
+        exit(json_encode(array('message'=>"未知错误！")));
+    }
 }
 else{
     http_response_code(405);
