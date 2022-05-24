2a1e56d7af07c3b5d81db74c6fb998c2a36fe479
xintianle <20307110196@fudan.edu.cn>
2022-05-25 00:34:57 +0800

    add recharge in user.php

--- php/user/user.php
+++ php/user/user.php
@@ -21,10 +21,32 @@ if ($req_method == "GET"){//获取用户信息
     http_response_code(200);
     exit(json_encode($data));
 }
-elseif ($req_method == "POST"){
+elseif ($req_method == "POST"){//充值
+    $data = json_decode(file_get_contents('php://input'), true);
+
+    $mysql = new Mysql();
+    $user = $mysql->selectACustomer($userID);
+    $account = $user->UserAccount;
+
+    $amount = $data['amount'];
+    if ($amount > 1000 || $account <= 0){
+        http_response_code(400);
+        exit(json_encode(array('message'=>"充值金额只能为1000以内的正整数！")));
+    }
+
+    $account = $account + $amount;
+
+    $map = array('UserAccount'=>$account);
+    $result = $mysql->update('customers', $map, "WHERE CustomerID='$userID'");
+
+    if (!$result){
+        http_response_code(500);
+        exit(json_encode(array('message'=>"未知错误！")));
+    }
 
     http_response_code(200);
-    //充值
+    exit(json_encode(array('message'=>"充值成功！")));
+
 }
 else{
     http_response_code(405);
