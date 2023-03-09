4949450d624a760225123916e9f371f6d6671e40
xintianle <20307110196@fudan.edu.cn>
2023-03-09 16:15:23 +0800

    debug

--- php/testttt.php
+++ php/testttt.php
@@ -1,5 +1,8 @@
 <?php
-require_once "Mysql.php";
+$conncet = mysqli_connect("121.37.100.255", "root", "123456", "art", "3306", "/tmp/mysql.sock");
+if (!$conncet) exit("connect fail");
+$result = mysqli_query($conncet, "SELECT * FROM customers");
+if (!$result) exit("result fail");
+$row = mysqli_fetch_row($result);
 
-$mysql = new Mysql();
-var_dump($mysql->selectAllShortPaintings());
+exit(json_encode(array('1'=>$row)));
